f59605165f86f7356d69ff06d4fbf0ebac98ed47
peterpan <apppeterpan@gmail.com>
2020-10-05 22:58:45 +0800

    Initial Commit

---
+++ DemoAccount/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoAccount
+//
+//  Created by SHIH-YING PAN on 2020/10/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoAccount/DemoAccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAccountApp.swift
+//  DemoAccount
+//
+//  Created by SHIH-YING PAN on 2020/10/5.
+//
+
+import SwiftUI
+
+@main
+struct DemoAccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
